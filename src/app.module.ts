98f6f159c54d7b52b1739fc10a85b7bc9392ed98
kingjinyong <wlsdydwhswkf@gmail.com>
2025-01-31 10:43:21 +0900

    chore: 도커 db 생성 후 로컬 서버에서 db 연동

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '1234',
+      database: 'stockdb',
+      autoLoadEntities: true, // 엔티티 자동 로드
+      synchronize: true, // 개발 환경에서만 true (자동 마이그레이션)],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
